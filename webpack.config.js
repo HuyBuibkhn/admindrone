732d072d9a78ae56ade71edb788b80f8ec5ba0f5
HuyBuibkhn <42404030+HuyBuibkhn@users.noreply.github.com>
2018-12-13 21:32:09 +0700

    upcong

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,7 @@ module.exports = {
         contentBase: './src/index.js',
         host: '0.0.0.0',
         compress: true,
-        port: 3000, // port number
+        port: 8080, // port number
         historyApiFallback: true,
         quiet: true
     },
